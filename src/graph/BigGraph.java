e1a5ddea4f28eb23fc72df93bb8bcc8242363287
Noah Garner <noahguy16@gmail.com>
2019-04-24 14:42:03 -0700

    Implemented minDomSet algorithm. Working on Facebook data, and working for small set data graphs.

--- src/graph/BigGraph.java
+++ src/graph/BigGraph.java
@@ -1,8 +1,8 @@
 
 package graph;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import graph.tester.CSVReader;
+
 import java.util.*;
 
 import static util.GraphLoader.loadGraph;
@@ -12,30 +12,25 @@ import static util.GraphLoader.loadGraph;
  */
 public class BigGraph implements Graph {
     //an integer that maps to a list of integers to mapnodes
-    HashMap<Integer, MapNode> myGraph;
+    HashMap<Integer, MapNode> graphNodes;
     //to track all edges between nodes in our graph
     HashSet<MapEdge> myGraphEdges;
-
     //construct our graph
     public BigGraph() {
-        this.myGraph = new HashMap<>();
+        this.graphNodes = new HashMap<>();
         this.myGraphEdges = new HashSet<>();
     }
 
-    /* (non-Javadoc)
-     * @see graph.Graph#addVertex(int)
-     */
     @Override
     public void addVertex(int num) throws IllegalArgumentException {
         //add a mapNode at this particular name, oh lets do error checking!
         //i.e. what if this guy is already in the map!
-        if (!!!myGraph.containsKey(num)) {
-            this.myGraph.put(num, new MapNode(num));
+        if (!!!graphNodes.containsKey(num)) {
+            this.graphNodes.put(num, new MapNode(num));
         } else {
             //System.out.println("num "+num+" was already added.");
         }
     }
-
     /* (non-Javadoc)
      * @see graph.Graph#addEdge(int, int)
      */
@@ -44,8 +39,8 @@ public class BigGraph implements Graph {
         //we need the node at this value
         //note to remember, each node has a set of edges i.e. friends when contstructed!!
         //what if 4 and 5 aren' in the map? Error check this
-        MapNode fromNode = myGraph.get(from);
-        MapNode toNode = myGraph.get(to);
+        MapNode fromNode = graphNodes.get(from);
+        MapNode toNode = graphNodes.get(to);
         if (fromNode == null || toNode == null)
             throw new NullPointerException(toNode.toString() + " is not in the graph");
 
@@ -60,16 +55,14 @@ public class BigGraph implements Graph {
     public HashSet<MapEdge> getEdges() {
         return this.myGraphEdges;
     }
-
     //get all the vertices in the graph
     public Stack<Integer> getAllIntegerVertices() {
         Stack<Integer> verticesAsStack = new Stack<>();
-        for (MapNode node : myGraph.values()) {
+        for (MapNode node : graphNodes.values()) {
             verticesAsStack.push(node.val);
         }
         return verticesAsStack;
     }
-
     /* (non-Javadoc)
      * @see graph.Graph#getEgonet(int)
      */
@@ -78,27 +71,25 @@ public class BigGraph implements Graph {
         //asked us to return a copy, lets makes a copy.
         //generate subgraph, copies of original graph nodes, return it
         BigGraph egonet = new BigGraph();
-        if (!this.myGraph.containsKey(center)) {
+        if (!this.graphNodes.containsKey(center)) {
             return egonet;
         }
         //put center into the egonet
         MapNode centerNode = new MapNode(center);
         egonet.addVertex(center);
         //put all of these nodes neighbors into the map
-        for (MapNode x : this.myGraph.get(center).getNodeFriends()) {
+        for (MapNode x : this.graphNodes.get(center).getNodeFriends()) {
             //create the web between center and friends
             egonet.addVertex(x.val);
             egonet.addEdge(centerNode.val, x.val);
         }
         //check the friend of friends of center to see if it has an edge
-
-        Set<Integer> egoNodes = egonet.myGraph.keySet();
-
+        Set<Integer> egoNodes = egonet.graphNodes.keySet();
         for (Integer i : egoNodes) {
             //skip center, we just added centers edges
             if (i == center)
                 continue;
-            for (MapNode n : this.myGraph.get(i).getNodeFriends()) {
+            for (MapNode n : this.graphNodes.get(i).getNodeFriends()) {
                 //does our set contain anything related to this element
                 if (egoNodes.contains(n.val)) {
                     egonet.addEdge(i, n.val);
@@ -107,7 +98,6 @@ public class BigGraph implements Graph {
         }
         return egonet;
     }
-
     //SCC: maximal, and SCC.
     @Override
     public List<Graph> getSCCs() {
@@ -142,36 +132,33 @@ public class BigGraph implements Graph {
         }
         return strongComponentList;
     }
-
     //part 1, build the stack
     public void DFS(int aNode, HashSet<Integer> visited, Stack<Integer> finished) {
         visited.add(aNode);
         //for each neighbor of the node
-        for (int neighbor : myGraph.get(aNode).getIntegerFriends()) {
+        for (int neighbor : graphNodes.get(aNode).getIntegerFriends()) {
             //this is the most important line of code in the entire algorithm and is what was causing so many issues
             //if the neighbor has not been visited, and the neighbor is an ENDNODE! (because we swapped the nodes) i.e. a = aNode, b = neighbor then a <- b
-            if (!visited.contains(neighbor) && myGraph.get(aNode).hasEdgeTo(myGraph.get(neighbor))) {
+            if (!visited.contains(neighbor) && graphNodes.get(aNode).hasEdgeTo(graphNodes.get(neighbor))) {
                 //call DFS again, and also, edit go into hasEdgeTo
                 DFS(neighbor, visited, finished);
             }
         }
         finished.push(aNode);
     }
-
     //part 2, build the component (implement comparable for nodeval)
     public void DFSUtil(int aNode, HashSet<Integer> visitedTranspose, BigGraph component) {
         visitedTranspose.add(aNode); //add 5, working
         component.addVertex(aNode); //add the vertex (has a set of edges)
         //this is where something is going wrong
-        for (Integer neighbor : myGraph.get(aNode).getIntegerFriends()) {
+        for (Integer neighbor : graphNodes.get(aNode).getIntegerFriends()) {
             //this is the most important line of code in the entire algorithm and is what was causing so many issues
             //if the neighbor has not been visited, and the neighbor is an ENDNODE! (because we swapped the nodes) i.e. a = aNode, b = neighbor then a <- b
-            if (!visitedTranspose.contains(neighbor) && myGraph.get(aNode).hasEdgeTo(myGraph.get(neighbor))) {
+            if (!visitedTranspose.contains(neighbor) && graphNodes.get(aNode).hasEdgeTo(graphNodes.get(neighbor))) {
                 DFSUtil(neighbor, visitedTranspose, component);
             }
         }
     }
-
     //swap all edges in the graph, for SCC method.
     public void swapEdges() {
         for (MapEdge edge : myGraphEdges) {
@@ -181,28 +168,109 @@ public class BigGraph implements Graph {
             edge.startNode = temp;
         }
     }
+    //start of the minDomSet Algorithm lets do this (skeleton)
+    public static Set<Integer> minDomSetOptimized(BigGraph graph){
+            //a dominating set
+            Set<Integer> aDomSet = new HashSet<>();
+            //uncover all vertices to start. working O(n)
+            uncoverVertices(graph);
+            //to track each node's in a table
+            HashMap<Integer, Integer> vertexTable = new HashMap<>();
+            //create look up table <Node,#neighbors> O(n)
+            for(MapNode aNode : graph.graphNodes.values()) {
+                vertexTable.put(aNode.getVal(), aNode.getNumNeighbors());
+            }
+            //init algorithm, we will break when all are marked
+            boolean hasUncovered = true;
+            while(hasUncovered) {
+                //create max <K,V> pair in table init null O(1) space, working
+                Map.Entry<Integer, Integer> maxEntry = null;
+                //find the max entry in the table O(n), working
+                for (Map.Entry<Integer, Integer> entry : vertexTable.entrySet()) {
+                    if (maxEntry == null || entry.getValue() > maxEntry.getValue())
+                        maxEntry = entry;
+                }
+                //get the graph vertex at maxEntry. O(1)
+                int currMaxVertex = maxEntry.getKey();
+                //remove max entry from the table O(1), working
+                p("CurrMaxVertex: "+maxEntry.getKey()+"\n"+"CurrMaxValue: "+maxEntry.getValue());
+                //remove this from the vertexTable, don't need for ref anymore
+                vertexTable.remove(currMaxVertex);
+                //cover the currMaxVertex O(1)..., working!!!
+                graph.graphNodes.get(currMaxVertex).setCovered(true);
+                //cover curMaxVertexs neighbors + update vertexTable!!!O(n)... working!!
+                graph.graphNodes.get(currMaxVertex).coverNodeNeighbors(vertexTable);
+                //add the currMaxEntry to the minDomSet
+                aDomSet.add(currMaxVertex);
+                //Do we have more covered nodes? Assume all are covered Time: O(n)
+                hasUncovered = false;
+                //check if any are uncovered
+                for (MapNode aNode : graph.graphNodes.values()) {
+                    //if we find one that is not covered... we have to run algorithm again
+                    if (!aNode.isCovered()) {
+                        hasUncovered = true;
+                        break;
+                    }
+                }
 
-    @Override
+
+                //print the coveredness of each vertex in the graph with lambda
+                graph.graphNodes.forEach((k, v) ->
+                        p("key: " + k + " Value:" + v.covered));
+                //print the updated HashTable of vertex and their neighbor#s
+                p("VertexTable State After update: \n"+vertexTable.keySet().toString() + "\n" + vertexTable.values().toString());
+                //
+                p("Curr minDomSet: " + aDomSet.toString());
+                //printnewline for new runthrough
+                p("");
+            }
+        return aDomSet;
+    }
+    //helper to init "uncover" vertices
+    public static void uncoverVertices(BigGraph graph) {
+        //iterate and uncover all nodes in the graph. JK this works intended
+        for(MapNode currNode : graph.graphNodes.values()) {
+            currNode.setCovered(false);
+        }
+
+    }
+
+    //for testing purposes
     public HashMap<Integer, HashSet<Integer>> exportGraph() {
         //init return map
         HashMap<Integer, HashSet<Integer>> convertedGraph = new HashMap<>();
         //now we should iterate over our map, and put all its neighbors into the hashmap's hashset vals
-        this.myGraph.forEach((key, value) -> convertedGraph.put(key, value.getIntegerFriends()));
+        this.graphNodes.forEach((key, value) -> convertedGraph.put(key, value.getIntegerFriends()));
         return convertedGraph;
     }
-
-
+    //program insertion point
     public static void main(String[] args) {
-        //prelim tests, notice we don't need to use third param
-        sccPrinter("data/scc/test_2.txt", true, 2);
-/*        big file lets go, Stackoverflow :(
-        sccPrinter("data/twitter_combined.txt");*/
-        sccPrinter("data/wikivotes", false, 4098);
-        //try twitter_higgs?
-        sccPrinter("data/twitter_higgs.txt",false,0);
+        String fileToLoad = "data/facebook_ucsd.txt";
+        //instantiate graph object
+        BigGraph fileGraph = new BigGraph();
+        //test SCC, note we cannot run this on entire data, so we run on an egonet
+        loadGraph(fileGraph, fileToLoad);
+        p("\n\n*****testing minDomFunction stuff*****\n\n");
+        //don't run this unless you have 6 minutes to waste, or you are not using facebook data
+        //facebook data is 886,000k edges, and 15,000 nodes
+        minDomSetOptimized(fileGraph);
+
+/*        egonet test working.
+        BigGraph egonet = fileGraph.getEgonet(22);
+
+        egonet.graphNodes.forEach((k,v) ->
+                p(k+" "+v));
+        minDomSetOptimized(egonet);*/
+        // 4333 nodes to cover a 15000 node graph. hmmm, i guess thats ok,seems bad
 
     }
 
+
+
+
+
+
+    //quick scc print
     public static void sccPrinter(String fileName, boolean getEgo, int egoVal)   {
         //instantiate graph object
         BigGraph graphDataFromFile = new BigGraph();
@@ -233,7 +301,7 @@ public class BigGraph implements Graph {
             p(anSCC.toString());
         }
     }
-
+    //quick print
     public static void p(String printStuff) {
         System.out.println(printStuff);
     }
--- src/graph/MapNode.java
+++ src/graph/MapNode.java
@@ -1,39 +1,64 @@
 package graph;
 
+import java.util.HashMap;
 import java.util.HashSet;
+
+import static graph.BigGraph.p;
 
 //represents a vertice, a name, but in number form for ease of use
 public class MapNode {
 
     //a node in the map has a value, and a set of edges coming out of it
     int val;//remember, names = numbers, this number represents a name
+
+    int numNeighbors;
     boolean covered; //have we visited this node yet for min dom set?
     HashSet<MapEdge> nodeEdgeSet;
 
     //construct our MapNode
     public MapNode(int val) {
         this.val = val;
+        //so we can access #neighbors in O(1) time.
+        this.numNeighbors = 0;
         this.covered = false;
         this.nodeEdgeSet = new HashSet<>();
     }
 
     //this could potentially confuse the shit out of users. Total anti-pattern, refactor later haha
-    public void setCovered() {
-        this.covered = !this.covered;
+    public void setCovered(boolean covered) {
+        this.covered = covered;
     }
 
-    public int getVal(){
+    public boolean isCovered() {
+        return this.covered;
+    }
+
+    public int getVal() {
         return val;
     }
 
     //what if we want to add an edge? 'a friendship'
-    public void addEdge(MapEdge edge){
+    //it is added as a toNode AND a fromNode... i.e. twice each time..
+    public void addEdge(MapEdge edge) {
         nodeEdgeSet.add(edge);
+        //this way we can access #neighbors in O(1) time instead of O(n)
+        this.numNeighbors++;
+        //if a node has a two, and a from, then subtract 1?
     }
 
+    //just in case, for the future if we need to remove friendships, etc...
+    public void removeEdge(MapEdge edge) {
+        nodeEdgeSet.remove(edge);
+        this.numNeighbors--;
+    }
+
+    //note, we do NOT want a setter for this, the setting is done above..
+    public int getNumNeighbors() {
+        return numNeighbors;
+    }
 
     //what if we want to get all the friends of this node? val = human name!?
-    public HashSet<MapNode> getNodeFriends(){
+    public HashSet<MapNode> getNodeFriends() {
         HashSet<MapNode> friends = new HashSet<>();
         for (MapEdge mapEdge : nodeEdgeSet) {
             //this is the current node
@@ -43,7 +68,7 @@ public class MapNode {
     }
 
     //specifically for the exportGraph Method because it wants integer vals
-    public HashSet<Integer> getIntegerFriends(){
+    public HashSet<Integer> getIntegerFriends() {
         HashSet<Integer> friends = new HashSet<>();
         for (MapEdge mapEdge : nodeEdgeSet) {
             //this is the current node
@@ -52,12 +77,60 @@ public class MapNode {
         return friends;
     }
 
+    //check cover function. This needs to be refactored
+    public void coverNodeNeighbors(HashMap<Integer, Integer> vertexTable) {
+        HashSet<MapNode> neighbors = new HashSet<>();
+        //get the neighbors Time O(n)
+        for (MapEdge mapEdge : nodeEdgeSet) {
+            //this is the current node
+            neighbors.add(mapEdge.getOtherNode(this));
+        }
+        //cover the neighbor. We start here for chadi's improvement. Working Time O(n)
+        //create array of neighbors to check
+        for (MapNode neighbor : neighbors) {
+            //get the key(neighbor) from vertexTable, see if it exists in vertexTable
+            if (vertexTable.containsKey(neighbor.getVal())) {
+                //make sure it exists
+                if (neighbor.covered == false) {
+                    //cover it
+                    neighbor.setCovered(true);
+                    //update vertex table
+                    vertexTable.put(neighbor.getVal(), vertexTable.get(neighbor.getVal()) - 2);
+                }
+                else
+                    //This node has been covered, remove from our indexing table.
+                    vertexTable.remove(neighbor.getVal());
+            }
+            else {
+                //its not in the table... therefore it must be covered... lmao idk..
+                neighbor.setCovered(true);
+            }
+        }
+        for (MapNode neighbor : neighbors) {
+            //get the key(neighbor) from vertexTable, see if it exists in vertexTable
+            if (vertexTable.containsKey(neighbor.getVal())) {
+                //make sure it exists
+                if (neighbor.covered == false) {
+                    //cover it
+                    neighbor.setCovered(true);
+                    //update vertex table
+                    vertexTable.put(neighbor.getVal(), vertexTable.get(neighbor.getVal()) - 2);
+                }
+                else
+                    //This node has been covered, remove from our indexing table.
+                    vertexTable.remove(neighbor.getVal());
+            }
+            else {
+                //its not in the table... therefore it must be covered... lmao idk..
+                neighbor.setCovered(true);
+            }
+        }
 
-
+    }
 
     @Override
     public String toString() {
-        return "value: "+val;
+        return "value: " + val;
     }
 
     //this is the function we needed to get our algorithm to work. Only works with twitter data,
@@ -68,7 +141,12 @@ public class MapNode {
                 return true;
             }
         }
-        return  false;
+        return false;
     }
+
+    //what the fuck
+
+
+
 }
 
